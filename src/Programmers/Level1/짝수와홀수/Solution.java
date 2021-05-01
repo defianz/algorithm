2aeb4f72cd07b4abbc1af1e5653f5ed7405f21a3
Defian <iautm91@gmail.com>
2021-05-01 17:28:00 +0900

    Algorithm Study

---
+++ src/Programmers/Level1/짝수와홀수/Solution.java
@@ -0,0 +1,9 @@
+package Programmers.Level1.짝수와홀수;
+
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        answer = num % 2 == 0 ? "Even" : "Odd";
+        return answer;
+    }
+}
---
+++ src/Programmers/Level1/체육복/Solution.java
@@ -0,0 +1,40 @@
+package Programmers.Level1.체육복;
+
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        boolean[] check = new boolean[reserve.length];
+        boolean[] clear = new boolean[lost.length];
+        answer = n - lost.length;
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if(reserve[j] == lost[i] && check[j] == false){
+                    check[j] = true;
+                    clear[i] = true;
+                    answer += 1;
+                    break;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < lost.length; i++) {
+            if(clear[i] == true) continue;
+            for (int j = 0; j < reserve.length; j++) {
+                if((reserve[j] == lost[i]-1 && check[j] == false) || (reserve[j] == lost[i]+1 && check[j] == false)){
+                    check[j] = true;
+                    answer += 1;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
