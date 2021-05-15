ac0b4e5c83dea9501d63486eafedacfe963376c8
Defian <iautm91@gmail.com>
2021-05-16 00:40:49 +0900

    Algorithm Study

---
+++ src/Programmers/Level1/삼진법뒤집기/Solution.java
@@ -0,0 +1,20 @@
+package Programmers.Level1.삼진법뒤집기;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        String tmp = "";
+        while(n != 0){
+            tmp += String.valueOf(n%3);
+            n /= 3;
+        }
+
+        int idx = 0;
+        for (int i = tmp.length()-1; i >= 0; i--) {
+            answer += Integer.parseInt(tmp.charAt(i)+"") * Math.pow(3,idx++);
+        }
+
+        return answer;
+    }
+}
