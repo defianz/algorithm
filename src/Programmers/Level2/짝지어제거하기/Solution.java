a0e6eb8a45bbabb49f6d0630ed779449a7f6606e
Defian <iautm91@gmail.com>
2021-05-27 18:23:13 +0900

    Algorithm Study

---
+++ src/Programmers/Level2/짝지어제거하기/Solution.java
@@ -0,0 +1,31 @@
+package Programmers.Level2.짝지어제거하기;
+
+import java.util.Stack;
+
+class Solution
+{
+    public int solution(String s)
+    {
+
+       Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char nxt = s.charAt(i);
+            if(st.empty()) st.add(nxt);
+            else {
+                if(st.peek() == nxt){
+                    st.pop();
+                } else {
+                    st.add(nxt);
+                }
+            }
+        }
+
+        return st.size() == 0 ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println("s.solution(\"baabaa\") = " + s.solution("baabaa"));
+        System.out.println("s.solution(\"cdcd\") = " + s.solution("cdcd"));
+    }
+}
---
+++ src/Programmers/Level2/캐시/Solution.java
@@ -0,0 +1,38 @@
+package Programmers.Level2.캐시;
+
+import java.util.LinkedList;
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+
+        LinkedList<String> ll = new LinkedList<>();
+
+        if(cacheSize==0) return cities.length *5;
+
+        for (int i = 0; i < cities.length; i++) {
+            String nxt = cities[i].toLowerCase();
+
+            if(ll.size() != 0 && ll.contains(nxt)){
+                ll.remove(nxt);
+                ll.add(nxt);
+                answer += 1;
+            } else if (ll.size() >= cacheSize ){
+                ll.removeFirst();
+                ll.add(nxt);
+                answer += 5;
+            } else {
+                ll.add(nxt);
+                answer += 5;
+            }
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println("s.solution(2,new String[]{\"Jeju\", \"Pangyo\", \"NewYork\", \"newyork\"}) = " + s.solution(2, new String[]{"Jeju", "Pangyo", "NewYork", "newyork"}));
+        System.out.println("s.solution(0,new String[]{\"Jeju\", \"Pangyo\", \"Seoul\", \"NewYork\", \"LA\"}) = " + s.solution(0,new String[]{"Jeju", "Pangyo", "Seoul", "NewYork", "LA"}));
+    }
+}
