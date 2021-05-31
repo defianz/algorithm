09024cfe3cb3e801fb19b62e5b51182709b2381c
Defian <iautm91@gmail.com>
2021-05-31 12:19:35 +0900

    Algorithm Study

--- src/Programmers/Level2/방금그곡/Solution_Fail.java
+++ src/Programmers/Level2/방금그곡/Solution_Fail.java
@@ -1,11 +1,12 @@
 package Programmers.Level2.방금그곡;
 
-class Solution_solving {
+class Solution_Fail {
     public String solution(String m, String[] musicinfos) {
         String answer = "(None)";
         int answerHM = 0;
 
         for(String musicInfo : musicinfos){
+
             String[] tmp = musicInfo.split(",");
             String[] st = tmp[0].split(":");
             String[] en = tmp[1].split(":");
@@ -21,12 +22,14 @@ class Solution_solving {
             int originAkLen = originAk.length();
 
             int idx =0;
+            // 고쳐야됨
             for (int i = 0; i < totalH; i++) {
-                char nxt = originAk.charAt(idx %originAkLen);
+                char nxt = originAk.charAt(idx % originAkLen);
                 if(nxt == '#' ) i-= 1;
                 idx++;
                 makeAk += nxt;
             }
+            if(originAk.charAt(idx %originAkLen) =='#') makeAk += originAk.charAt(idx % originAkLen);
             System.out.println("makeAk = " + makeAk);
 
             if(makeAk.contains(m)){
@@ -47,8 +50,11 @@ class Solution_solving {
     }
 
     public static void main(String[] args) {
-        Solution_solving s = new Solution_solving();
+        Solution_Fail s = new Solution_Fail();
+//        System.out.println(s.solution("ABCDEFG", new String[]{"12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"}));
+        System.out.println(s.solution("CC#BCC#BCC#BCC#B", new String[]{"03:00,03:02,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"}));
+        System.out.println(s.solution("ABC", new String[]{"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"}));
 //        System.out.println("s.solution(\"ABCDEFG\",new String[]{\"12:00,12:14,HELLO,CDEFGAB\", \"13:00,13:05,WORLD,ABCDEF\"}) = " + s.solution("ABCDEFG", new String[]{"12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"}));
-        System.out.println("s.solution(\"ABC\",new String[]{\"12:00,12:14,HELLO,C#DEFGAB\", \"13:00,13:05,WORLD,ABCDEF\"}) = " + s.solution("ABC", new String[]{"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"}));
+//        System.out.println("s.solution(\"ABC\",new String[]{\"12:00,12:14,HELLO,C#DEFGAB\", \"13:00,13:05,WORLD,ABCDEF\"}) = " + s.solution("ABC", new String[]{"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"}));
     }
 }
